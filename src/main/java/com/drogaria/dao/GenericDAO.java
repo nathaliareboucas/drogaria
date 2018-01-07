0ae66cd82914c7b8383068e0ca7fa8c593749ac5
Nathalia <nathreboucas5@gmail.com>
2018-01-07 15:15:22 -0300

    Criando os métodos buscar, excluir e editar no genéricDAO e testes dos métodos no estadoDAOTest

--- src/main/java/com/drogaria/dao/GenericDAO.java
+++ src/main/java/com/drogaria/dao/GenericDAO.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
 import com.drogaria.util.HibernateUtil;
 
@@ -47,6 +48,58 @@ public class GenericDAO<Entidade> {
 		} catch (RuntimeException erro) {
 			throw erro;
 		}finally {
+			sessao.close();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Entidade buscar(Long codigo) {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		
+		try {
+			Criteria consulta = sessao.createCriteria(classe);
+			consulta.add(Restrictions.idEq(codigo));
+			Entidade resultado = (Entidade) consulta.uniqueResult();
+			return resultado;
+		} catch (RuntimeException erro) {
+			throw erro;
+		}finally {
+			sessao.close();
+		}
+	}
+	
+	public void excluir(Entidade entidade) {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		Transaction transacao = null;
+
+		try {
+			transacao = sessao.beginTransaction();
+			sessao.delete(entidade);
+			transacao.commit();
+		} catch (RuntimeException erro) {
+			if (transacao != null) {
+				transacao.rollback();
+			}
+			throw erro;
+		} finally {
+			sessao.close();
+		}
+	}
+	
+	public void editar(Entidade entidade) {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		Transaction transacao = null;
+
+		try {
+			transacao = sessao.beginTransaction();
+			sessao.update(entidade);
+			transacao.commit();
+		} catch (RuntimeException erro) {
+			if (transacao != null) {
+				transacao.rollback();
+			}
+			throw erro;
+		} finally {
 			sessao.close();
 		}
 	}
--- src/test/java/com/drogaria/dao/EstadoDAOTest.java
+++ src/test/java/com/drogaria/dao/EstadoDAOTest.java
@@ -8,25 +8,75 @@ import org.junit.Test;
 import com.drogaria.domain.Estado;
 
 public class EstadoDAOTest {
-	
+
 	@Test
 	@Ignore
 	public void salvar() {
 		Estado estado = new Estado();
-		estado.setNome("São Paulo");
-		estado.setSigla("SP");
-		
+		estado.setNome("Ceará");
+		estado.setSigla("CE");
+
 		EstadoDAO estadoDAO = new EstadoDAO();
 		estadoDAO.salvar(estado);
+		System.out.println("Registro salvo com sucesso.");
 	}
-	
+
 	@Test
+	@Ignore
 	public void listar() {
 		EstadoDAO estadoDAO = new EstadoDAO();
 		List<Estado> resultado = estadoDAO.listar();
-		
-		for (Estado estado:resultado) {
+
+		for (Estado estado : resultado) {
 			System.out.println(estado.getSigla() + " - " + estado.getNome());
+		}
+	}
+
+	@Test
+	@Ignore
+	public void buscar() {
+		Long codigo = 3L;
+
+		EstadoDAO estadoDAO = new EstadoDAO();
+		Estado estado = estadoDAO.buscar(codigo);
+
+		if (estado == null) {
+			System.out.println("Nenhum registro encontrado.");
+		} else {
+			System.out.println(estado.getSigla() + " - " + estado.getNome());
+		}
+
+	}
+
+	@Test
+	@Ignore
+	public void excluir() {
+		Long codigo = 4L;
+
+		EstadoDAO estadoDAO = new EstadoDAO();
+		Estado estado = estadoDAO.buscar(codigo);
+		
+		if (estado == null) {
+			System.out.println("Nenhum registro encontrado.");
+		} else {
+			estadoDAO.excluir(estado);
+			System.out.println("registro removido com sucesso.");
+		}
+	}
+	
+	@Test
+	public void editar() {
+		Long codigo = 3L;
+
+		EstadoDAO estadoDAO = new EstadoDAO();
+		Estado estado = estadoDAO.buscar(codigo);
+		
+		if (estado == null) {
+			System.out.println("Nenhum registro encontrado.");
+		}else {
+			estado.setSigla("CE");
+			estadoDAO.editar(estado);
+			System.out.println("Registro editado com sucesso.");
 		}
 	}
 
