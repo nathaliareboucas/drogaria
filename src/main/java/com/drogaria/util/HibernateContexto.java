3c53532bcf09394f5fa9cd7a5595dc979cd7cea6
Nathalia <nathreboucas5@gmail.com>
2018-01-21 14:45:41 -0300

    ServletContextListener - Inicializando hibernate junto com o tomcat

---
+++ src/main/java/com/drogaria/util/HibernateContexto.java
@@ -0,0 +1,19 @@
+package com.drogaria.util;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class HibernateContexto implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		HibernateUtil.getFabricaDeSessoes().close();
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		HibernateUtil.getFabricaDeSessoes();
+		
+	}
+
+}
