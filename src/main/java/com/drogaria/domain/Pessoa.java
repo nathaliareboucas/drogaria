9510292d732e16887bf32272de3fc6c01ac82a49
Nathalia <nathreboucas5@gmail.com>
2018-02-22 10:28:40 -0300

    Inclusão do campo cidade na entidade Pessoa

--- src/main/java/com/drogaria/domain/Pessoa.java
+++ src/main/java/com/drogaria/domain/Pessoa.java
@@ -2,6 +2,8 @@ package com.drogaria.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @SuppressWarnings("serial")
 @Entity
@@ -30,6 +32,10 @@ public class Pessoa extends GenericDomain {
 
 	@Column(length = 10)
 	private String complemento;
+
+	@ManyToOne
+	@JoinColumn(nullable = false)
+	private Cidade cidade;
 
 	@Column(length = 13, nullable = false)
 	private String telefone;
@@ -102,6 +108,14 @@ public class Pessoa extends GenericDomain {
 
 	public void setComplemento(String complemento) {
 		this.complemento = complemento;
+	}
+
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
 	}
 
 	public String getTelefone() {
