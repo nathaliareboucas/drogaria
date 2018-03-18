94afecea80c08963a9b2ed3a5feac549205a1664
Nathalia <nathreboucas5@gmail.com>
2018-03-18 20:07:21 -0300

    salvar e listar funcionário

---
+++ src/main/java/com/drogaria/bean/FuncionarioBean.java
@@ -0,0 +1,87 @@
+package com.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+
+import com.drogaria.dao.FuncionarioDAO;
+import com.drogaria.dao.PessoaDAO;
+import com.drogaria.domain.Funcionario;
+import com.drogaria.domain.Pessoa;
+
+@ManagedBean
+@ViewScoped
+public class FuncionarioBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Funcionario funcionario;
+	private List<Funcionario> funcionarios;
+	private FuncionarioDAO funcionarioDAO;
+	private List<Pessoa> pessoas;
+	private PessoaDAO pessoaDAO;
+
+	@PostConstruct
+	public void init() {
+		funcionarioDAO = new FuncionarioDAO();
+		listar();
+	}
+
+	public void listar() {
+		try {
+			funcionarios = funcionarioDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Erro ao listar os funcionários");
+			erro.printStackTrace();
+		}
+	}
+	
+	public void salvar() {
+		try {		
+			funcionarioDAO.merge(funcionario);
+			Messages.addGlobalInfo("Funcionário salvo com sucesso");
+			novo();
+			listar();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Erro ao salvar o funcionário");
+			erro.printStackTrace();
+		}
+	}
+
+	public void novo() {
+		funcionario = new Funcionario();
+		listarPessoas();
+	}
+	
+	public void listarPessoas() {
+		try {
+			pessoaDAO = new PessoaDAO();
+			pessoas = pessoaDAO.listar();
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Erro ao listar as pessoas");
+			erro.printStackTrace();
+		}
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+}
